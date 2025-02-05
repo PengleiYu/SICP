0687f1dd3ee6ddd95de7198e55ce898568a859ab
yupenglei <yupengniaosanshi@gmail.com>
2025-02-05 20:20:11 +0800

    代码优化

--- chapter1/page1.2.2.js
+++ chapter1/page1.2.2.js
@@ -23,19 +23,36 @@
     console.log(fib1(10))
     console.log(fib2(10))
 
+    // 我们有一些50美分、25美分、10美分、5美分和1美分的硬币，要把1美元换成零钱，一共有多少种不同的方式？
+    // 更一般的问题是，给了任意数量的美元现金，我们能写出一个程序，计算出所有换零钱方式的数目吗？
     // 换零钱，树形递归的应用
     function count_change(amount) {
-        // 假设零钱种类已按币值排序，
         function first_denomination(kinds_of_coins) {
-            return kinds_of_coins === 1 ? 1
-                : kinds_of_coins === 2 ? 5
-                    : kinds_of_coins === 3 ? 10
-                        : kinds_of_coins === 4 ? 25
-                            : kinds_of_coins === 5 ? 50
-                                : 0;
+            // 假设零钱种类已按币值排序，
+            // let result = kinds_of_coins === 1 ? 1
+            //     : kinds_of_coins === 2 ? 5
+            //         : kinds_of_coins === 3 ? 10
+            //             : kinds_of_coins === 4 ? 25
+            //                 : kinds_of_coins === 5 ? 50
+            //                     : 0;
+            // 其实零钱顺序不重要，只要保持顺序固定即可
+            const arr = [50, 5, 1, 10, 25,];
+            let result = arr[kinds_of_coins - 1] ?? 0;
+            console.log(`first_denomination(${kinds_of_coins}) return ${result}`)
+            return result;
         }
 
+        /**
+         * 把总数为a的现金换成n种硬币的不同方式的数目等于
+         * 现金a换成除去第一种硬币之外的所有其他硬币的不同方式数目
+         * 加上现金a-d换成所有种类的硬币的不同方式数目，其中d是第一种硬币的币值
+         *
+         * @param amount{number}
+         * @param kinds_of_coins{number}
+         * @returns {number}
+         */
         function cc(amount, kinds_of_coins) {
+            console.log(`cc() called: amount=${amount},kinds_of_coins=${kinds_of_coins}`)
             return amount === 0
                 ? 1
                 : amount < 0 || kinds_of_coins === 0
